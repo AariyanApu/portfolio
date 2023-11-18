ce5e8b83d37277d6cc3431a84dc98077c08081eb
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-18 19:35:16 +0600

    fix footer credit

--- src/components/FooterCredit.tsx
+++ src/components/FooterCredit.tsx
@@ -1,16 +1,14 @@
-import { titleFont } from "@/utils/fonts";
-
 const FooterCredit = () => {
   return (
     <div
       className={`mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-4 pb-5    text-[11px] text-slate-color sm:items-end sm:justify-end sm:px-6   sm:text-xs lg:px-8`}
     >
-      <p className="text-center leading-normal ">
+      <p className='text-center leading-normal '>
         @2023 Design & Built by Aariyan
       </p>
       <a
-        href="https://github.com/AariyanApu/aariyanapu.com"
-        className=" color_animation hover:text-primary-color"
+        href='https://github.com/AariyanApu/aariyanapu.com'
+        className=' color_animation hover:text-primary-color'
       >
         No Copyright. Code is free to use & Open-Source
       </a>
