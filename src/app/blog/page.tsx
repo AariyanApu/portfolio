43d36297118b1033903375571c16fa5500c8b87a
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-22 08:40:09 +0600

    Fix padding in Blog component and update footer credit

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import { blogPosts } from '@/utils/data';
 
 export default function Blog() {
   return (
-    <div className='mx-auto max-w-5xl flex flex-col gap-y-8 sm:gap-y-4'>
+    <div className='mx-auto max-w-5xl flex flex-col gap-y-8 sm:gap-y-4 py-8'>
       {blogPosts?.map((item) => (
         <BlogCard item={item} key={item.id} />
       ))}
--- src/components/FooterCredit.tsx
+++ src/components/FooterCredit.tsx
@@ -1,7 +1,9 @@
 const FooterCredit = () => {
   return (
     <div
-      className={`mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-4 pb-5    text-[11px] text-slate-color sm:items-end sm:justify-end sm:px-6   sm:text-xs lg:px-8 mt-2`}
+      className={
+        'mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-4 pb-5    text-[11px] text-slate-color sm:items-end sm:justify-end sm:px-6   sm:text-xs lg:px-8 mt-2'
+      }
     >
       <p className='text-center leading-normal '>
         @2023 Design & Built by Aariyan
