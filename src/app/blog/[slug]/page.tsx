6ee8b0a60f89aa97962bde96b6f5073ffe35a341
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-23 17:15:19 +0600

    Add @tailwindcss/typography as a dev dependency

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,10 @@
 // import { getData, getDataNoStore, getDataNoStoreLocal } from '@/utils/getData'
+import { getDataNoStoreLocal } from '@/utils/getData';
 import Image from 'next/image';
 
 export default async function SingleBlog({ params }: any) {
   const { slug } = params;
-  // const data = await getDataNoStore(`posts/${slug}`)
+  const data = await getDataNoStoreLocal(`posts/${slug}`);
 
   return (
     <div className={`mx-auto my-4 max-w-5xl px-4 lg:px-0 `}>
@@ -16,34 +17,17 @@ export default async function SingleBlog({ params }: any) {
       />
 
       <div className='flex flex-col gap-y-6'>
-        {/* <p className="mt-8">{data.createdAt.substring(0, 10)}</p> */}
-        <h1 className='text-2xl sm:text-3xl'>How to use React Context API</h1>
-        {/* <div
-          className="md:text-sm lg:text-base"
+        <div className=' mt-8 flex flex-row gap-x-20'>
+          <p className=''> CREATED AT : {data.createdAt.substring(0, 10)}</p>
+          <p className=''>VIEWED BY : {data.views} PEOPLE.</p>
+        </div>
+        <h1 className='text-2xl sm:text-3xl'>{data.title}</h1>
+        <div
+          className='md:text-sm lg:text-base prose text-slate-100  prose-headings:text-slate-50 prose-lg:text-slate-50'
           dangerouslySetInnerHTML={{
             __html: data.desc,
           }}
-        /> */}
-
-        <div className=''>
-          React Context API is a way to essentially create global variables that
-          can be passed around in a React app. This is the alternative to prop
-          drilling, or passing props from grandparent to parent to child, and so
-          on.
-          <br />
-          <br />
-          Context is often touted as a simpler, lighter solution to using Redux
-          for state management. However, context can only be used for simple
-          state management, and Redux is still recommended for complex state
-          management.
-          <br />
-          <br />
-          In this tutorial, we’ll be using the useContext Hook in React to
-          create a global state management system without needing to use Redux.
-          <br />
-          <br />
-          Let’s get started!
-        </div>
+        />
       </div>
     </div>
   );
