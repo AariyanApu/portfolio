d2728bbeb0fd76da03a855d30071904ee60bfafb
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-23 16:13:37 +0600

    Add async/await to fetch data in Blog component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,12 +1,14 @@
 import BlogCard from '@/components/BlogCard';
 import { blogPosts } from '@/utils/data';
+import { getDataNoStoreLocal } from '@/utils/getData';
 
-export default function Blog() {
+export default async function Blog() {
+  const data = await getDataNoStoreLocal('posts');
+  console.log(data);
   return (
     <div className='mx-auto max-w-5xl flex flex-col gap-y-8 sm:gap-y-4 py-8'>
-      {blogPosts?.map((item) => (
-        <BlogCard item={item} key={item.id} />
-      ))}
+      {Array.isArray(data) &&
+        data?.map((item) => <BlogCard item={item} key={item.id} />)}
     </div>
   );
 }
