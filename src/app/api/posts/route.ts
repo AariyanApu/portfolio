30fee214af807330feedfe38b9d99114dfe0bb81
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-23 16:02:40 +0600

    add post route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,37 @@
+import prisma from '@/utils/connect';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  try {
+    const posts = await prisma.post.findMany();
+    return new NextResponse(JSON.stringify(posts), { status: 200 });
+  } catch (error) {
+    return new NextResponse('Database Error', { status: 500 });
+  }
+};
+export const POST = async (req: Request) => {
+  // const session = await getAuthSession();
+
+  // if (!session) {
+  //   return new NextResponse(
+  //     JSON.stringify({ message: 'Not Authenticated!' }, { status: 401 })
+  //   );
+  // }
+
+  try {
+    const body = await req.json();
+    const posts = await prisma.post.create({
+      data: body,
+    });
+
+    return new NextResponse(JSON.stringify(posts));
+    // return new NextResponse(JSON.stringify({ status: 200 }), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({
+        message: 'Something went wrong with fucking database!',
+      })
+    );
+  }
+};
