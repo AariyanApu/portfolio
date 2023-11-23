050fef4f0acfd2bedc97f654b7ea6317bb836e93
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-23 16:17:04 +0600

    Refactor BlogCard component

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,8 @@
 import BlogCard from '@/components/BlogCard';
-import { blogPosts } from '@/utils/data';
 import { getDataNoStoreLocal } from '@/utils/getData';
 
 export default async function Blog() {
   const data = await getDataNoStoreLocal('posts');
-  console.log(data);
   return (
     <div className='mx-auto max-w-5xl flex flex-col gap-y-8 sm:gap-y-4 py-8'>
       {Array.isArray(data) &&
--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,26 +1,20 @@
 import { blogPostType } from '@/types/dataTypes';
 import Image from 'next/image';
 import Link from 'next/link';
+import CloudImage from './utility/CloudImage';
 
 export default function BlogPostCard({ item }: { item: blogPostType }) {
   return (
     <div
       key={item.id}
-      className='mx-4 flex flex-col justify-around rounded-md bg-secondary-color transition-all duration-500 ease-in-out hover:bg-[#132444] sm:flex-row xl:mx-0  overflow-hidden border-2 border-secondary-color hover:border-[#1d3666]'
+      className='mx-4 flex flex-col justify-start gap-x-16 rounded-md bg-secondary-color transition-all duration-500 ease-in-out hover:bg-[#132444] sm:flex-row xl:mx-0  overflow-hidden border-2 border-secondary-color hover:border-[#1d3666]'
     >
-      {/* <CloudImage
+      <CloudImage
         src={item.imgUrl}
         alt={item.title}
         height={500}
         width={500}
-        customStyles="h-72 w-[550px] rounded-t-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100 sm:rounded-s-md sm:rounded-tr-none md:h-80"
-      /> */}
-      <Image
-        src={item.imgUrl}
-        alt={item.title}
-        height={500}
-        width={500}
-        className='h-72 w-[550px] rounded-t-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100 sm:rounded-s-md sm:rounded-tr-none md:h-80   '
+        customStyles='h-72 w-[550px] rounded-t-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100 sm:rounded-s-md sm:rounded-tr-none md:h-80   '
       />
       <div className=' mt-10 flex flex-col gap-y-2 pb-1 pl-1 sm:pl-8 px-4'>
         <p className='mt-4 text-sm'>{item.createdAt.substring(0, 10)}</p>
