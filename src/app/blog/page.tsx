6fb4a0bfb8841b7cddddf41ccab14ec2a4d6c8a7
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-24 15:55:42 +0600

    Refactor blog page and single blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,12 @@
-// import BlogCard from '@/components/BlogCard';
-// import { getDataNoStore } from '@/utils/getData';
+import BlogCard from '@/components/BlogCard';
+import { getDataNoStore } from '@/utils/getData';
 
-// export default async function Blog() {
-//   const data = await getDataNoStore('posts');
-//   return (
-//     <div className='mx-auto max-w-5xl flex flex-col gap-y-8 sm:gap-y-4 py-8'>
-//       {Array.isArray(data) &&
-//         data?.map((item) => <BlogCard item={item} key={item.id} />)}
-//     </div>
-//   );
-// }
-import React from 'react';
-
-export default function page() {
-  return <div>page</div>;
+export default async function Blog() {
+  const data = await getDataNoStore('posts');
+  return (
+    <div className='mx-auto max-w-5xl flex flex-col gap-y-8 sm:gap-y-4 py-8'>
+      {Array.isArray(data) &&
+        data?.map((item) => <BlogCard item={item} key={item.id} />)}
+    </div>
+  );
 }
