71402a30f7c3a59d2bb8fdf01619ec3acc761a21
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-25 00:36:31 +0600

    update footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,46 +1,44 @@
-import type { Metadata } from 'next';
-import { Quicksand } from 'next/font/google';
-import './globals.css';
-import NavBar from '@/components/NavBar';
-import FooterCredit from '@/components/FooterCredit';
-import AuthProvider from '@/utils/AuthProvider';
+import type { Metadata } from "next";
+import { Quicksand } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import FooterCredit from "@/components/FooterCredit";
+import AuthProvider from "@/utils/AuthProvider";
 
-const quickSand = Quicksand({ subsets: ['latin'], weight: ['400', '700'] });
+const quickSand = Quicksand({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
   title: "Aaru's World!",
 
-  description: 'Aariyan Apu is a full-stack Web Developer From Bangladesh.',
+  description: "Aariyan Apu is a full-stack Web Developer From Bangladesh.",
   keywords: [
-    'aariyan',
-    'aaru',
-    'আরিয়ান',
-    'আরিয়ান অপু',
-    'arian opu',
-    'arian',
-    'arian opu',
-    'aaruni',
-    'aariyan apu',
-    'aariyan',
-    'aariyan apu web developer',
-    'aariyan apu web developer from bangladesh',
-    'aariyan apu js developer',
-    'aariyan apu next.js developer',
-    'aariyan apu react developer',
-    'aariyan apu react',
-    'aariyan apu bangladeshwebdeveloper',
-    'aariyan apu aariyan',
-    'aariyan apu web dev from bd',
-    'aariyan apu aaru',
-    'web developer',
-    'web developer from bangladesh',
-    'js developer',
-    'next.js developer',
-    'react developer',
+    "aariyan",
+    "aaru",
+    "আরিয়ান",
+    "আরিয়ান অপু",
+    "arian opu",
+    "arian",
+    "arian opu",
+    "aaruni",
+    "aariyan apu",
+    "aariyan",
+    "aariyan apu web developer",
+    "aariyan apu web developer from bangladesh",
+    "aariyan apu js developer",
+    "aariyan apu next.js developer",
+    "aariyan apu react developer",
+    "aariyan apu react",
+    "aariyan apu bangladeshwebdeveloper",
+    "aariyan apu aariyan",
+    "aariyan apu web dev from bd",
+    "aariyan apu aaru",
+    "web developer",
+    "web developer from bangladesh",
+    "js developer",
+    "next.js developer",
+    "react developer",
   ],
-  viewport:
-    'minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover',
-  metadataBase: new URL('https://aariyanapu.com'),
+  metadataBase: new URL("https://aariyanapu.com"),
 };
 
 export default function RootLayout({
@@ -49,7 +47,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body
         className={`${quickSand.className} mx-auto max-w-screen-2xl text-[#e6ebff]`}
       >
--- src/components/FooterCredit.tsx
+++ src/components/FooterCredit.tsx
@@ -2,15 +2,15 @@ const FooterCredit = () => {
   return (
     <div
       className={
-        'mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-4 pb-5    text-[11px] text-slate-color sm:items-end sm:justify-end sm:px-6   sm:text-xs lg:px-8 mt-2'
+        "mx-auto flex w-full max-w-7xl flex-col items-center justify-center px-4 pb-5    text-[11px] text-slate-color sm:items-end sm:justify-end sm:px-6   sm:text-xs lg:px-8 mt-2"
       }
     >
-      <p className='text-center leading-normal '>
-        @2023 Design & Built by Aariyan
+      <p className="text-center leading-normal ">
+        @2024 Design & Built by Aariyan
       </p>
       <a
-        href='https://github.com/AariyanApu/aariyanapu.com'
-        className=' color_animation hover:text-primary-color'
+        href="https://github.com/AariyanApu/aariyanapu.com"
+        className=" color_animation hover:text-primary-color"
       >
         No Copyright. Code is free to use & Open-Source
       </a>
