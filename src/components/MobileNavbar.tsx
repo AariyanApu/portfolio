271accc0d8e0c55e6ad7a66ea5d8c2ca52cbbe52
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-27 01:25:26 +0600

    change mobile icon

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -1,38 +1,39 @@
-"use client";
-import { navigation } from "@/utils/data";
-import Link from "next/link";
-import { useState } from "react";
-import { FaBars, FaXmark } from "react-icons/fa6";
+'use client';
+import { navigation } from '@/utils/data';
+import Link from 'next/link';
+import { useState } from 'react';
+import { CgMenuRightAlt } from 'react-icons/cg';
+import { FaXmark } from 'react-icons/fa6';
 
 export default function MobileNavbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <div className=" absolute inset-2   z-20 sm:hidden">
+      <div className=' absolute inset-2   z-20 sm:hidden'>
         {!isOpen ? (
-          <FaBars
+          <CgMenuRightAlt
             // make on click previous value reverse
             onClick={() => setIsOpen(!isOpen)}
-            className="slate-color cursor-pointer "
+            className='slate-color cursor-pointer '
             size={35}
           />
         ) : (
           <FaXmark
             onClick={() => setIsOpen(!isOpen)}
-            className="slate-color cursor-pointer overscroll-none "
+            className='slate-color cursor-pointer overscroll-none '
             size={35}
           />
         )}
       </div>
       {isOpen && (
         <>
-          <div className="absolute inset-0 z-10 flex h-screen w-full flex-col items-center justify-center gap-y-8 overscroll-none bg-background-color">
+          <div className='absolute inset-0 z-10 flex h-screen w-full flex-col items-center justify-center gap-y-8 overscroll-none bg-background-color'>
             {navigation.map((item) => (
               <Link
                 href={item.href}
                 key={item.name}
                 onClick={() => setIsOpen(false)}
-                className="text-4xl  hover:text-primary-color"
+                className='text-4xl  hover:text-primary-color'
               >
                 {item.name}
               </Link>
