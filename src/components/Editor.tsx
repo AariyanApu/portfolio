04bfa3fd5b3684fd95ecd97e2b695e040aeb24e2
Aariyan Apu <aariyan.apu@gmail.com>
2024-04-01 02:06:42 +0600

    change text editor

---
+++ src/components/Editor.tsx
@@ -0,0 +1,63 @@
+'use client';
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+
+export default function TextEditor({
+  desc,
+  setDesc,
+}: {
+  desc: string;
+  setDesc: any;
+}) {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      //   console.log((editorRef.current as any).getContent());
+      setDesc((editorRef.current as any).getContent());
+    }
+  };
+  return (
+    <>
+      <Editor
+        apiKey='goguvd9rjb71i2z7xk6lijncr14qjtvfgsvnd3nypne4o6di'
+        onInit={(evt, editor) => (editorRef.current = editor as any)}
+        initialValue={desc}
+        init={{
+          height: 1000,
+          width: '100%',
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+      <button onClick={log} className='rounded-md bg-sky-400 px-4 py-2'>
+        Submit Blog
+      </button>
+    </>
+  );
+}
