6b3a64c9225b0c2be8408d74a304b2229f8e3481
Aariyan Apu <aariyan.apu@gmail.com>
2024-06-25 04:29:25 +0600

    add dynamic og image

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,11 @@
 import CloudImage from "@/components/utility/CloudImage";
 import { getDataNoStore } from "@/utils/getData";
 import DOMPurify from "isomorphic-dompurify";
-
-import { getCldImageUrl } from "next-cloudinary";
 import { Metadata } from "next";
 import { getCldOgImageUrl } from "next-cloudinary";
 
 export async function generateMetadata({ params }: any): Promise<Metadata> {
+  // @ts-ignore
   const { slug } = params;
   const data = await getDataNoStore(`posts/${slug}`);
 
@@ -20,16 +19,16 @@ export async function generateMetadata({ params }: any): Promise<Metadata> {
         {
           // Prefer a different size? Be sure to update the width and height of the
           // metadata as well as the image configuration of getCldOgImageUrl
-          width: 1200,
-          height: 630,
+          width: 2400,
+          height: 1260,
           url: getCldOgImageUrl({
             src: publicId,
             effects: [{ colorize: "100,co_black" }],
             overlays: [
               {
                 publicId,
-                width: 1200,
-                height: 630,
+                width: 2400,
+                height: 1260,
                 crop: "fill",
                 effects: [
                   {
@@ -90,10 +89,10 @@ export default async function SingleBlog({ params }: any) {
       />
 
       <div className="flex w-full flex-col gap-y-6">
-        <div className=" mt-8 flex flex-row gap-x-20">
-          <p className=""> CREATED AT : {data.createdAt.substring(0, 10)}</p>
-          <p className="">VIEWED : {data.views} TIMES.</p>
-        </div>
+        {/* <div className=" mt-8 flex flex-row gap-x-20"> */}
+        {/*   <p className=""> CREATED AT : {data.createdAt.substring(0, 10)}</p> */}
+        {/*   <p className="">VIEWED : {data.views} TIMES.</p> */}
+        {/* </div> */}
         <h1 className="text-2xl font-semibold sm:text-4xl">{data.title}</h1>
         <div
           className="prose-base prose-slate prose-invert    prose-a:text-sky-400 hover:prose-a:text-sky-500 prose-img:my-8 prose-img:w-auto prose-img:overflow-hidden prose-img:rounded-md prose-img:object-cover prose-img:object-center prose-img:shadow-md  md:text-sm lg:text-lg prose-img:lg:h-96"
