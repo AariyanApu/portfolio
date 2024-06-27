d8286bc8ad86f48539a85538e90461f597ab3d4d
Aariyan Apu <aariyan.apu@gmail.com>
2024-06-27 20:36:30 +0600

    fix loading image issue

--- src/components/utility/Loading.tsx
+++ src/components/utility/Loading.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Loading() {
   return (
     <Image
-      alt='loading......'
+      alt="loading......"
       height={500}
       width={500}
-      className='mx-auto mb-60 mt-28 h-44 w-44 animate-pulse  transition duration-300'
-      src='/android-chrome-384x384.png'
+      className="mx-auto mb-60 mt-28 h-44 w-44 animate-pulse  transition duration-300"
+      src="/icons/icon.png"
     />
   );
 }
